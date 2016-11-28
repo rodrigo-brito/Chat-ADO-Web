6738812ccdaa693af2c61eb5005fee8ae03fd47b
Rodrigo Brito <rig.10@hotmail.com>
2016-11-28 10:43:47 -0200

    Correções no carregamento dos scripts js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
-	// Get a reference to the database service
-	var database = firebase.database();
-	var mensagens = firebase.database().ref().child('mensagens');
+	// Referência da base do Firebase
+	var mensagens = firebase.database().ref('mensagens');
 
+	//Quando nova mensagem é salva
 	mensagens.on('child_added', function(data) {
 		addComentario(data.val());
 	});
-	//var commentsRef = firebase.database().ref('');
+
+	/**
+	 * Adiciona nova mensagem na UI do Chat
+	 * @param {JSON} comentario
+	 */
 	function addComentario(comentario){
 		var msg_element = $('<div class="row message-bubble"></div>');
 		msg_element.append('<p class="text-muted">'+comentario.nome+' ('+comentario.date+')</p>');
@@ -15,6 +19,7 @@ $(document).ready(function(){
 		window.scrollTo(0,document.body.scrollHeight);
 	}
 
+	//Click para enviar mensagem
 	$("#btn-enviar").click(function(){
 		var msg = {
 			nome: $('#input-nome').val(),
@@ -25,6 +30,7 @@ $(document).ready(function(){
 		$('#mensagem').val("");
 	});
 
+	//Enter no botão de enviar mensagem
 	$('#form-mensagem').on('submit', function(event){
 		event.preventDefault();
 		var msg = {
@@ -36,7 +42,10 @@ $(document).ready(function(){
 		$('#mensagem').val("");
 	})
 
-
+	/**
+	 * Adiciona zero para unidade de tempo menor que 10
+	 * @param {String} tempo com zero incluso
+	 */
 	function addZero(i) {
 		if (i < 10) {
 			i = "0" + i;
@@ -44,6 +53,10 @@ $(document).ready(function(){
 		return i;
 	}
 
+	/**
+	 * Retorna as horas e minutos formatados
+	 * @return {String} Hora e minuto no formato HH:mm
+	 */
 	function getHoraMinuto() {
 		var d = new Date();
 		var h = addZero(d.getHours());
